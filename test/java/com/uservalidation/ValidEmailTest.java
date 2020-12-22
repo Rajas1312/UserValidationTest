37fe916a42496f8d8d6b11056a6179cf473ad764
Rajas1312 <you@example.com>
2020-12-22 21:35:12 +0530

    Added: testing the email test case

---
+++ test/java/com/uservalidation/ValidEmailTest.java
@@ -0,0 +1,33 @@
+package com.uservalidation;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
+public class ValidEmailTest {
+    public String emailCheck;
+    public boolean expectedResult;
+
+    public ValidEmailTest(String emailCheck,boolean expectedResult){
+        this.emailCheck=emailCheck;
+        this.expectedResult=expectedResult;
+
+    }
+    @Parameterized.Parameters
+    public static Collection data(){
+        return Arrays.asList(new Object [][]{{"itsemerajas2@gmail.com",true},{"rajas333dongre@gmail.com",true}});
+
+    }
+
+    @Test
+    public void givenEmail_WhenProperOrImproper_ShouldReturnTrueOrFalse() {
+        UserValidation userValidation = new UserValidation();
+        boolean result=userValidation.emailIdValidate(this.emailCheck);
+        Assert.assertEquals(this.expectedResult,result);
+    }
+}
